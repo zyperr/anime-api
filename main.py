b06678f03f3f2a09f34178435b3811a6d9b8e499
zyperr <ramerica200@gmail.com>
2023-09-14 03:56:56 -0300

    corrigiendo nombre de archivo requirement a requirements

--- main.py
+++ main.py
@@ -2,8 +2,6 @@ from fastapi import FastAPI
 from config.database import engine_db,Base
 from middlewares.error_handler import ErrorHandler 
 from routers.anime import anime_router
-import os
-import uvicorn
 
 
 
@@ -13,7 +11,3 @@ app.version = "0.0.1"
 app.add_middleware(ErrorHandler)
 app.include_router(anime_router)
 Base.metadata.create_all(bind=engine_db)
-
-if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0",
-                port=int(os.environ.get("PORT", 8000)))
