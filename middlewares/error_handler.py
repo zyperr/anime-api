b7c266e271cea039028f3cc0cc8fee5b5e908626
zyperr <ramerica200@gmail.com>
2023-09-14 02:44:09 -0300

    Haciendo cambios para subir a railway

--- middlewares/error_handler.py
+++ middlewares/error_handler.py
@@ -1,13 +1,13 @@
 from starlette.middleware.base import BaseHTTPMiddleware, RequestResponseEndpoint
 from fastapi import FastAPI,Request,Response
 from fastapi.responses import JSONResponse
-
+from typing import Union
 
 
 class ErrorHandler(BaseHTTPMiddleware):
     def __init__(self, app: FastAPI) -> None:
         super().__init__(app)
-    async def dispatch(self, request: Request, call_next: RequestResponseEndpoint) -> Response | JSONResponse:
+    async def dispatch(self, request: Request, call_next: RequestResponseEndpoint) -> Union[Response,JSONResponse]:
         try:
             return await call_next(request)
         except Exception as err:
