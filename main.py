2a849eb563326ec5ab5118184ebc3bcc7aae7777
zyperr <ramerica200@gmail.com>
2023-09-14 03:07:54 -0300

    arreglando..

--- main.py
+++ main.py
@@ -15,4 +15,4 @@ app.include_router(anime_router)
 Base.metadata.create_all(bind=engine_db)
 
 if __name__ == "__main__":
-    uvicorn.run("main:app",host="0.0.0.0",port=(os.environ.get("PORT",8000)))
+    uvicorn.run("main:app", host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
