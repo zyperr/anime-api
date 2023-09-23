3bb4546ebeecdd5600854d8fceeb2f16dc387ce5
zyperr <ramerica200@gmail.com>
2023-09-23 16:32:46 -0300

    Agregando cors

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 from fastapi import FastAPI
 from config.database import engine_db,Base
-from middlewares.error_handler import ErrorHandler 
+from middlewares.error_handler import ErrorHandler
 from routers.anime import anime_router
 from routers.user import login_router
-
-
+from fastapi.middleware.cors import CORSMiddleware
 
 
 app = FastAPI()
@@ -14,7 +13,13 @@ app.add_middleware(ErrorHandler)
 app.include_router(anime_router)
 app.include_router(login_router)
 
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 Base.metadata.create_all(bind=engine_db)
 
 
